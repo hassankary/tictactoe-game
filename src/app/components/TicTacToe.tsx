8e3b6745599c6d24992d672f351a139ff2cd5f5e
Hassan Kaeru <hassanaskary29@gmail.com>
2024-09-02 18:39:28 +0700

    updated: conditional & logic

--- src/app/components/TicTacToe.tsx
+++ src/app/components/TicTacToe.tsx
@@ -5,38 +5,56 @@ import { useEffect, useState } from "react";
 export default function TicTacToe() {
   const [count, setCount] = useState<number>(0);
   const [lock, setLock] = useState<boolean>(false);
-  const [data, setData] = useState<string[]>(["", "", "", "", "", "", "", "", ""]);
+  const [data, setData] = useState<string[]>([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+
+  //  [0, 1, 2]
+  //  [3, 4, 5]
+  //  [6, 7, 8]
 
   useEffect(() => {
     console.log("data =>", data);
-  }, [data]);
+    console.log("lock =>", lock);
+    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data[0]);
+    } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
+      won(data[3]);
+    } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
+      won(data[6]);
+    } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
+      won(data[0]);
+    } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
+      won(data[1]);
+    } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
+      won(data[2]);
+    } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
+      won(data[0]);
+    } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
+      won(data[2]);
+    }
+  }, [data, lock]);
 
   const toggle = (e: React.MouseEvent<HTMLDivElement>, num: number) => {
     e.preventDefault();
     if (lock) {
       return;
     }
-    if (count % 2 === 0) {
-      const newData = data.map((d, i) => {
-        if (i === num) {
-          return "x";
-        } else {
-          return d;
-        }
-      });
-      setData(newData);
-      setCount(count + 1);
-    } else {
-      const newData = data.map((d, i) => {
-        if (i === num) {
-          return "o";
-        } else {
-          return d;
-        }
-      });
-      setData(newData);
-      setCount(count + 1);
-    }
+
+    const newData = data.map((d, i) =>
+      i === num && d === "" ? (count % 2 === 0 ? "x" : "o") : d
+    );
+
+    setData(newData);
+    setCount(count + 1);
   };
 
   const resetHandler = () => {
@@ -45,6 +63,12 @@ export default function TicTacToe() {
     });
     setData(filteredData);
     setCount(0);
+    setLock(false);
+  };
+
+  const won = (winner: string) => {
+    console.log("winner =>", winner);
+    setLock(true);
   };
 
   return (
@@ -62,7 +86,9 @@ export default function TicTacToe() {
               <div
                 onClick={(e) => toggle(e, i)}
                 key={i}
-                className={`${d === "x" ? "text-emerald-400" : "text-orange-300"} flex justify-center items-center text-5xl bg-slate-800 rounded-md h-[100px] w-[100px] md:h-[160px] md:w-[160px]`}
+                className={`${
+                  d === "x" ? "text-emerald-400" : "text-orange-300"
+                } flex justify-center items-center text-5xl bg-slate-800 rounded-md h-[100px] w-[100px] md:h-[160px] md:w-[160px] transition-all duration-500 ease-in-out`}
               >
                 {d}
               </div>
